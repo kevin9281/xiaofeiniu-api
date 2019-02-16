fbc9b9fabb80218d9f2dd25632f27d3288389b1f
YanJunsong <121282320@qq.com>
2019-02-16 16:15:36 +0800

    创建数据库脚本文件xiaofeiniu.sql

---
+++ xiaofeiniu.sql
@@ -0,0 +1,6 @@
+SET NAMES UTF8;          /* 设置名称为UTF8 */
+DROP DATABASE IF EXISTS  /* 如果存在就丢弃xiaofeiniu */
+xiaofeiniu;
+CREATE DATABASE xiaofeiniu  /* 创建表xiaofeiniu */
+CHARSET=UTF8;   /* 表示当前文档的字符集是采用utf-8的字符,也就是我们常说英文字符集 */
+USE xiaofeiniu;
