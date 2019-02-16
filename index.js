d9c27728a7f9ecedc8cefd59b86d84b8d34f3100
YanJunsong <121282320@qq.com>
2019-02-16 18:44:07 +0800

    小肥牛扫码点餐系统-API

---
+++ index.js
@@ -0,0 +1,9 @@
+/* 小肥牛扫码点餐项目API子系统 */
+const PORT = 8090;    /* 端口号 */
+const express = require('express'); /* 引入模块 */
+ 
+/* 启动服务器 npm start */
+var app = express();  /* 用express 创建app */
+app.listen(PORT,()=>{     /* 监听端口 */
+	console.log('Server Listening'+PORT+'...');
+})
